9cec109b0f008e96b75b3aa1da57dd3e608eadd7
yangshuochina <yangshuochina@gmail.com>
2019-04-18 18:46:44 +0800

    add 0002-addTwoNumbers c code.

--- algorithms/c/addTwoNumbers/addTwoNumbers.c
+++ algorithms/c/addTwoNumbers/addTwoNumbers.c
@@ -16,6 +16,42 @@
  *     struct ListNode *next;
  * };
  */
+#include "stdlib.h"
+#include "main.h"
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    
+    struct ListNode *ret, *node;
+    struct ListNode *p = 0;
+    int carry = 0;
+    int i;
+
+    ret = 0;
+    i = 0;
+    while (l1 || l2) {
+        node = malloc(sizeof(struct ListNode));
+        node->next = 0;
+        if (0 == i) {
+            ret = node;
+            p = node;
+        } else {
+            p->next = node;
+            p = p->next;
+        }
+
+
+        node->val = ((l1? l1->val:0) + (l2? l2->val:0) + carry) % 10;
+        carry = ((l1? l1->val:0) + (l2? l2->val:0) + carry) / 10;
+
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
+        i++;
+    }
+
+    if (carry) {
+        node = malloc(sizeof(struct ListNode));
+        node->next = 0;
+        node->val = carry;
+        p->next = node;
+    }
+
+    return ret;
 }
